fc56f9abb341c404c2b93c23af9d76212483f4c4
Aaron Mahan <aaron@forerunner.games>
2018-05-19 19:03:05 -0700

    Automate more application installations.
    - Use HomeBrew to install Google Chrome & Slack.
    - Add Google Chrome, Slack, & MacVim to the Dock after brewing.

--- brew.sh
+++ brew.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+THIS_DIR="${BASH_SOURCE%/*}"
+[[ ! -d "$THIS_DIR" ]] && THIS_DIR="$PWD"
+[[ $(type -t "add_to_dock") != function ]] && . "$THIS_DIR/functions.sh"
+
 # Install Homebrew (Should automatically trigger the prerequisite installation
 # of Xcode Command Line Tools, if they aren't already installed. Another way to
 # install them is to open Xcode for the first time.)
@@ -19,6 +23,8 @@ brew cask install macvim --override-system-vim # get MacVim.app in /Applications
 brew install findutils --with-default-names # get cool 'find' options like -printf
 brew install duti # easy custom file associations
 brew cask install java8 # Java 8 development
+brew cask install google-chrome # Web browsing, why not? :)
+brew cask install slack # You shouldn't work alone...
 
 # Update system shell to use the new Bash.
 echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
@@ -27,3 +33,8 @@ exec bash
 
 # Configure custom file assocations with Duti.
 duti settings.duti
+
+# Put freshly brewed applications in the Dock.
+add_to_dock /Applications/Google\ Chrome.app
+add_to_dock /Applications/MacVim.app
+add_to_dock /Applications/Slack.app
--- functions.sh
+++ functions.sh
@@ -27,3 +27,12 @@ app_uti() {
   mdls -name kMDItemContentTypeTree "$f"
   rm "$f"
 }
+
+add_to_dock() {
+  if [[ ! -z $1 ]]; then
+    defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>$1</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"; killall Dock
+  else
+    printf "\nError: No application path specified, exiting.\n\n"
+    return 1
+  fi
+}
