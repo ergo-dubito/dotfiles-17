301dbb4b3b83159046eb62b436d07f37af6cd7cb
Aaron Mahan <aaron@forerunner.games>
2018-06-19 20:08:12 -0700

    Sort downloads folder by date added, not kind.
    - Makes it easier to find new downloads on top.

--- dock.sh
+++ dock.sh
@@ -37,7 +37,7 @@ dockutil --add /Applications/Messages.app
 dockutil --add /Applications/Reminders.app
 dockutil --add /Applications/Notes.app
 dockutil --add /Applications/System\ Preferences.app
-dockutil --add ~/Downloads --section others --view list --display folder --sort kind --replacing 'Downloads' # It's already there; just change its layout
+dockutil --add ~/Downloads --section others --view list --display folder --sort dateadded --replacing 'Downloads' # It's already there; just change its layout
 dockutil --add ~/Sync/Business/Forerunner\ Games/peril/scripts/aaron/peril-scripts --section others --view list --display folder --sort name
 dockutil --add ~/Sync/Business/Forerunner\ Games/peril/documents/peril-todo.txt --section others
 dockutil --add ~/Sync/Personal/software/dotfiles/dotfiles-todo.txt --section others
